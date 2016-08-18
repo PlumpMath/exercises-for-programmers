911ad171be5a017db0b414b56cfad185ea3521ec
bakyeono <bakyeono@gmail.com>
2016-08-18 13:19:08 +0900

    Solve ex 4

---
+++ python/ex-4-extend.py
@@ -0,0 +1,9 @@
+# Mad Libs
+# 확장 버전
+
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+adverb1 = input("Enter an adverb: ")
+adverb2 = input("Enter another adverb: ")
+print("Do you {0} your {1} {2} {3}? That's {4}!".format(verb, adjective, noun, adverb1, adverb2))
