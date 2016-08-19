432db5a6e9153ca68c59ba4781723920134029ec
bakyeono <bakyeono@gmail.com>
2016-08-19 17:49:32 +0900

    Solve ex 6

---
+++ python/ex-6.py
@@ -0,0 +1,15 @@
+# 퇴직 계산기
+
+import datetime
+
+current_age = int(input('What is yout current age? '))
+retire_age = int(input('At what age would you like to retire? '))
+left_years = retire_age - current_age
+current_year = datetime.date.today().year
+retire_year = current_year + left_years
+
+print('You have ' + str(left_years) + ' years left until you can retire.')
+print('It\'s ' + str(current_year) + ', ' +
+      'so you can retire in ' + str(retire_year) + '.')
+
+
